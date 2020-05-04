4d5f0f4f2d8716dfcf3184d26c2036392a0ce1f8
wujiawei1207537021 <41567516+wujiawei1207537021@users.noreply.github.com>
2020-05-04 11:03:57 +0800

    初始化项目

---
+++ src/main/java/com/example/dataway/DatawayApplication.java
@@ -0,0 +1,17 @@
+package com.example.dataway;
+
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableHasor()      // 在Spring 中启用 Hasor
+@EnableHasorWeb()   // 将 hasor-web 配置到 Spring 环境中，Dataway 的 UI 是通过 hasor-web 提供服务。
+@SpringBootApplication
+public class DatawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawayApplication.class, args);
+	}
+
+}
